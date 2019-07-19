f1294da15d3d53f1192e1192dcbb689f0d311d1f
Maksim Andrianov <maksimandrianov1@gmail.com>
2019-07-19 14:16:30 +0300

    +

--- generator/polygonizer.hpp
+++ generator/polygonizer.hpp
@@ -3,101 +3,220 @@
 #include "generator/borders.hpp"
 #include "generator/feature_builder.hpp"
 #include "generator/generate_info.hpp"
+#include "generator/osm_source.hpp"
+
+#include "indexer/feature_visibility.hpp"
+#include "indexer/cell_id.hpp"
 
 #include "geometry/rect2d.hpp"
-#include "geometry/mercator.hpp"
 
-#include <memory>
+#include "coding/file_writer.hpp"
+
+#include "base/base.hpp"
+#include "base/buffer_vector.hpp"
+#include "base/macros.hpp"
+
 #include <string>
-#include <utility>
-#include <vector>
+
+#ifndef PARALLEL_POLYGONIZER
+#define PARALLEL_POLYGONIZER 1
+#endif
+
+#if PARALLEL_POLYGONIZER
+#include <QtCore/QSemaphore>
+#include <QtCore/QThreadPool>
+#include <QtCore/QMutex>
+#include <QtCore/QMutexLocker>
+#endif
 
 namespace feature
 {
-// Groups features according to country polygons.
-template <class FeatureOut>
-class Polygonizer
-{
-public:
-  Polygonizer(feature::GenerateInfo const & info)
-    : m_info(info)
+  // Groups features according to country polygons
+  template <class FeatureOutT>
+  class Polygonizer
   {
-    if (info.m_splitByPolygons)
+    feature::GenerateInfo const & m_info;
+
+    vector<FeatureOutT*> m_Buckets;
+    vector<std::string> m_Names;
+    borders::CountriesContainer m_countries;
+
+#if PARALLEL_POLYGONIZER
+    QThreadPool m_ThreadPool;
+    QSemaphore m_ThreadPoolSemaphore;
+    QMutex m_EmitFeatureMutex;
+#endif
+
+  public:
+    Polygonizer(feature::GenerateInfo const & info)
+      : m_info(info)
+#if PARALLEL_POLYGONIZER
+      , m_ThreadPoolSemaphore(m_ThreadPool.maxThreadCount() * 8)
+#endif
     {
-      CHECK(borders::LoadCountriesList(info.m_targetDir, m_countries),
-            ("Error loading country polygons files."));
+#if PARALLEL_POLYGONIZER
+      LOG(LINFO, ("Polygonizer thread pool threads:", m_ThreadPool.maxThreadCount()));
+#endif
+
+      if (info.m_splitByPolygons)
+      {
+        CHECK(borders::LoadCountriesList(info.m_targetDir, m_countries),
+            ("Error loading country polygons files"));
+      }
+      else
+      {
+        // Insert fake country polygon equal to whole world to
+        // create only one output file which contains all features
+        m_countries.Add(borders::CountryPolygons(info.m_fileName), MercatorBounds::FullRect());
+      }
     }
-    else
+    ~Polygonizer()
     {
-      // Insert fake country polygon equal to whole world to
-      // create only one output file which contains all features.
-      borders::RegionsContainer regions;
-      auto const rect = MercatorBounds::FullRect();
-      std::vector<m2::PointD> points {rect.LeftBottom(), rect.LeftTop(), rect.RightTop(),
-            rect.RightBottom(), rect.LeftBottom()};
-      regions.Add(m2::RegionD(std::move(points)), rect);
-      auto countries = borders::CountryPolygons(info.m_fileName, regions);
-      m_countries.Add(std::move(countries), rect);
-    }
-  }
-
-  ~Polygonizer()
-  {
-    Finish();
-  }
-
-  void operator()(FeatureBuilder & fb)
-  {
-    m_countries.ForEachInRect(fb.GetLimitRect(), [&](auto const & countryPolygons) {
-      auto const need = fb.ForAnyGeometryPoint([&](auto const & point) {
-        return countryPolygons.Contains(point);
-      });
-
-      if (need)
-        this->EmitFeature(countryPolygons, fb);
-    });
-  }
-
-  void Start()
-  {
-    m_currentNames.clear();
-  }
-
-  void Finish()
-  {
-  }
-
-  void EmitFeature(borders::CountryPolygons const & countryPolygons, FeatureBuilder const & fb)
-  {
-    if (countryPolygons.m_index == -1)
-    {
-      m_names.push_back(countryPolygons.GetName());
-      m_buckets.emplace_back(new FeatureOut(m_info.GetTmpFileName(countryPolygons.GetName())));
-      countryPolygons.m_index = static_cast<int>(m_buckets.size()) - 1;
+      Finish();
+      for_each(m_Buckets.begin(), m_Buckets.end(), base::DeleteFunctor());
     }
 
-    if (!m_currentNames.empty())
-      m_currentNames += ';';
+    struct PointChecker
+    {
+      borders::RegionsContainer const & m_regions;
+      bool m_belongs;
 
-    m_currentNames += countryPolygons.GetName();
-    m_buckets[countryPolygons.m_index]->Collect(fb);
-  }
+      PointChecker(borders::RegionsContainer const & regions)
+        : m_regions(regions), m_belongs(false) {}
 
-  std::vector<std::string> const & GetNames() const
-  {
-    return m_names;
-  }
+      bool operator()(m2::PointD const & pt)
+      {
+        m_regions.ForEachInRect(m2::RectD(pt, pt),
+                                std::bind<void>(std::ref(*this), std::placeholders::_1, std::cref(pt)));
+        return !m_belongs;
+      }
 
-  std::string const & GetCurrentNames() const
-  {
-    return m_currentNames;
-  }
+      void operator() (borders::Region const & rgn, borders::Region::Value const & point)
+      {
+        if (!m_belongs)
+          m_belongs = rgn.Contains(point);
+      }
+    };
 
-private:
-  feature::GenerateInfo const & m_info;
-  std::vector<std::unique_ptr<FeatureOut>> m_buckets;
-  std::vector<std::string> m_names;
-  borders::CountriesContainer m_countries;
-  std::string m_currentNames;
-};
+    class InsertCountriesPtr
+    {
+      typedef buffer_vector<borders::CountryPolygons const *, 32> vec_type;
+      vec_type & m_vec;
+
+    public:
+      InsertCountriesPtr(vec_type & vec) : m_vec(vec) {}
+      void operator() (borders::CountryPolygons const & c)
+      {
+        m_vec.push_back(&c);
+      }
+    };
+
+    void operator()(FeatureBuilder1 & fb)
+    {
+      buffer_vector<borders::CountryPolygons const *, 32> vec;
+      m_countries.ForEachInRect(fb.GetLimitRect(), InsertCountriesPtr(vec));
+
+      switch (vec.size())
+      {
+      case 0:
+        break;
+      case 1:
+        EmitFeature(vec[0], fb);
+        break;
+      default:
+        {
+#if PARALLEL_POLYGONIZER
+          m_ThreadPoolSemaphore.acquire();
+          m_ThreadPool.start(new PolygonizerTask(this, vec, fb));
+#else
+        PolygonizerTask task(this, vec, fb);
+        task.RunBase();
+#endif
+        }
+      }
+    }
+
+    std::string m_currentNames;
+
+    void Start()
+    {
+      m_currentNames.clear();
+    }
+
+    void Finish()
+    {
+#if PARALLEL_POLYGONIZER
+      m_ThreadPool.waitForDone();
+#endif
+    }
+
+    void EmitFeature(borders::CountryPolygons const * country, FeatureBuilder1 const & fb)
+    {
+#if PARALLEL_POLYGONIZER
+      QMutexLocker mutexLocker(&m_EmitFeatureMutex);
+      UNUSED_VALUE(mutexLocker);
+#endif
+      if (country->m_index == -1)
+      {
+        m_Names.push_back(country->m_name);
+        m_Buckets.push_back(new FeatureOutT(m_info.GetTmpFileName(country->m_name)));
+        country->m_index = static_cast<int>(m_Buckets.size())-1;
+      }
+
+      if (!m_currentNames.empty())
+        m_currentNames += ';';
+      m_currentNames += country->m_name;
+
+      auto & bucket = *(m_Buckets[country->m_index]);
+      bucket(fb);
+    }
+
+    vector<std::string> const & Names() const
+    {
+      return m_Names;
+    }
+
+  private:
+    friend class PolygonizerTask;
+
+    class PolygonizerTask
+#if PARALLEL_POLYGONIZER
+      : public QRunnable
+#endif
+    {
+    public:
+      PolygonizerTask(Polygonizer * pPolygonizer,
+                      buffer_vector<borders::CountryPolygons const *, 32> const & countries,
+                      FeatureBuilder1 const & fb)
+        : m_pPolygonizer(pPolygonizer), m_Countries(countries), m_fb(fb)
+      {
+      }
+
+      void RunBase()
+      {
+        for (size_t i = 0; i < m_Countries.size(); ++i)
+        {
+          PointChecker doCheck(m_Countries[i]->m_regions);
+          m_fb.ForEachGeometryPoint(doCheck);
+
+          if (doCheck.m_belongs)
+            m_pPolygonizer->EmitFeature(m_Countries[i], m_fb);
+        }
+      }
+
+#if PARALLEL_POLYGONIZER
+      void run()
+      {
+        RunBase();
+
+        m_pPolygonizer->m_ThreadPoolSemaphore.release();
+      }
+#endif
+
+    private:
+      Polygonizer * m_pPolygonizer;
+      buffer_vector<borders::CountryPolygons const *, 32> m_Countries;
+      FeatureBuilder1 m_fb;
+    };
+  };
 }  // namespace feature
